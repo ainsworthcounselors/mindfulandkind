f405dd1c3f5ae8f633fd4457ff2402172578e517
Grace Casale <gracecasale@gmail.com>
2018-12-05 20:21:07 -0500

    updated with uuid in mongo note model - protects users from knowing it's on mongo

--- server/models/note.js
+++ server/models/note.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
+const uuid = require('uuid');
 const noteSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-  },
-  coords: {
-    type: Object,
-    required: true
-  },
-  CreatedAt: {
-    type: Date,
-    required: true,
-    default: Date.now
-  },
-  UpdatedAt: {
-    type: Date,
-    required: true,
-    default: Date.now
-  }
+    id: {
+        type: String,
+        default: uuid.v4
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    coords: {
+        type: Object,
+        required: true
+    },
+    CreatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    UpdatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('Note', noteSchema);
