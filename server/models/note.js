70d8c6e20847206721f4742ad9350b91ce70907c
Grace Casale <gracecasale@gmail.com>
2018-12-05 20:21:30 -0500

    removed trailing coma

--- server/models/note.js
+++ server/models/note.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
 const uuid = require('uuid');
 const noteSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        default: uuid.v4
-    },
-    text: {
-        type: String,
-        required: true
-    },
-    coords: {
-        type: Object,
-        required: true
-    },
-    CreatedAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-    UpdatedAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    }
+  id: {
+    type: String,
+    default: uuid.v4
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  coords: {
+    type: Object,
+    required: true
+  },
+  CreatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  UpdatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Note', noteSchema);
