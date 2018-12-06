9533ac9804ceed04673190d6bbcc5885986f4e18
Grace Casale <gracecasale@gmail.com>
2018-12-05 19:07:26 -0500

    added a mongoose model for the database. updated the notes routes to utilize async/await instead of standard promises

--- server/models/note.js
+++ server/models/note.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: true,
-    },
-    coords: {
-        type: Object,
-        required: true
-    },
-    CreatedAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-    UpdatedAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    }
+  text: {
+    type: String,
+    required: true,
+  },
+  coords: {
+    type: Object,
+    required: true
+  },
+  CreatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  UpdatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Note', noteSchema);
