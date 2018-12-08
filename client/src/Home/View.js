44072ee95bab0dfbe9be6d2143d15d25530423c5
Grace Casale <gracecasale@gmail.com>
2018-12-08 13:58:48 -0500

    removed location services and updated text

--- client/src/Home/View.js
+++ client/src/Home/View.js
@@ -5,11 +5,11 @@ import Modal from "react-modal";
 
 Modal.setAppElement(document.getElementById("root"));
 
-const ErrorView = () => {
-  return (
-    <h1>You need to allow Geolocation Services to use the app.</h1>
-  )
-}
+// const ErrorView = () => {
+//   return (
+//     <h1>Error.</h1>
+//   )
+// }
 
 export default class HomeView extends Component {
   state = {
@@ -20,20 +20,20 @@ export default class HomeView extends Component {
     this.setState({ isOpen: !isOpen });
   };
   render() {
-    const { coords, error } = this.props;
-    const { latitude, longitude } = coords;
+    // const { coords, error } = this.props;
+    // const { latitude, longitude } = coords;
     const { isOpen } = this.state;
-    if(error) return <ErrorView />
+    // if(error) return <ErrorView />
     return (
       <React.Fragment>
         <section className="flex flex-column flex-grow-1 justify-content-center align-items-center ">
-          <p className="fw4 m-1">Latitude: {latitude}</p>
-          <p className="fw4 m-1">Longitude: {longitude}</p>
+          {/* <p className="fw4 m-1">Latitude: {latitude}</p>
+          <p className="fw4 m-1">Longitude: {longitude}</p> */}
           <button 
             onClick={this.toggleModal}
-            disabled={!(latitude && longitude)}
+            // disabled={!(latitude && longitude)}
             className="fw4 input-reset p-4 m-1"
-          >Note this Location</button>
+          >Submit a Mindful {"&"} Kind Note</button>
         </section>
         <Modal
           onRequestClose={this.toggleModal}
